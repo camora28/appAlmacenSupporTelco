0ba8e46fbee3280f7aad01c8a83b287d6a22f3b8
Camora28@ <camora88@gmail.com>
2020-09-23 04:23:00 -0500

    Servidor express configurado segun sistema adquiere puerto configurado por la variable(PORT) de entorno configurada o toma el puerto 4000 por default;

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+
+//Initializations
+const app = express();
+
+
+//Settings
+app.set('port', process.env.PORT || 4000);
+
+
+//Middlewares
+app.use(morgan('dev'));
+
+//Global Variables
+
+//Routes
+app.use(require('./routes'));
+
+
+//Public
+
+
+//Starting the servidor
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
+
+
