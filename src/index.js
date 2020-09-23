15815abf0383bb204c8877d68cff56381671cfe4
Camora28@ <camora88@gmail.com>
2020-09-23 04:52:58 -0500

    finalizacion Servidor y motor de plantillas con handlebars

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
 
 //Initializations
 const app = express();
@@ -7,19 +10,34 @@ const app = express();
 
 //Settings
 app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'))
+app.engine('.hbs', exphbs ({
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, 'layouts'),
+    extname: '.hbs',
+    helpers: require('./lib/hadlebars')
+}));
+app.set('view engine', '.hbs');
 
 
 //Middlewares
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 //Global Variables
+app.use((req, res, next) => {
+    next();
+})
 
 //Routes
 app.use(require('./routes'));
+app.use(require('./routes/authentication'));
+app.use(require('./routes/links')); 
 
 
 //Public
-
+app.use(express.static(path.join(__dirname, 'public')))
 
 //Starting the servidor
 app.listen(app.get('port'), () => {
