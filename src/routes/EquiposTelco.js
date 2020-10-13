21fd15e54b067ea7a362ce862583148accaaa67e
Camora28@ <camora88@gmail.com>
2020-10-13 06:22:59 -0500

    encapsulación de los datos y envio a base datos

--- src/routes/EquiposTelco.js
+++ src/routes/EquiposTelco.js
@@ -7,8 +7,19 @@ router.get('/add', (req, res) => {
     res.render('./equiposTelco/add');
 });
 
-router.post('/add', (req, res) => {
-    console.log(req.body);
+router.post('/add', async(req, res) => {
+    const {Id_ActivoTx, Serial_EqTx, Mac_EqTx, Modelo_EqTx, Marca_EqTx,Description_EqTx, Propietario_EqTx, Ubicación_EqTx } = req.body;
+    const newEquipoTx = {
+        Id_ActivoTx, 
+        Serial_EqTx, 
+        Mac_EqTx, 
+        Modelo_EqTx, 
+        Marca_EqTx, 
+        Description_EqTx, 
+        Propietario_EqTx, 
+        Ubicación_EqTx 
+    }
+    await pool.query('INSERT INTO equipostelco set ?', [newEquipoTx] );
     res.send('recivido');
 });
 
