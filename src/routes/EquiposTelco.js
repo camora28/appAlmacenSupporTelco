d34284d39668547f34c94254f6266f7989782da8
Camora28@ <camora88@gmail.com>
2020-11-26 06:28:33 -0500

    creacion de ruta profile y vista. se protegen rutas con el metodo isLoggedIn, isNotLoggedIn,

--- src/routes/EquiposTelco.js
+++ src/routes/EquiposTelco.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 // requiero la conexion a DB
 const pool = require('../database');
+const { isLoggedIn } = require('../lib/auth');
 
-router.get('/add', (req, res) => {
+router.get('/add', isLoggedIn, (req, res) => {
     res.render('./equiposTelco/add');
 });
 
 //agregamos toda la informacion del formulario en base de datos
-router.post('/add', async(req, res) => {
+router.post('/add', isLoggedIn,  async(req, res) => {
     const {Id_ActivoTx, Serial_EqTx, Mac_EqTx, Modelo_EqTx, Marca_EqTx,Description_EqTx, Propietario_EqTx, Ubicación_EqTx } = req.body;
     const newEquipoTx = {
         Id_ActivoTx, 
@@ -25,27 +26,27 @@ router.post('/add', async(req, res) => {
     res.redirect('/equiposTelco');
 });
 //consultamos la todos los equipos de la base de datos
-router.get('/', async(req, res) => {
+router.get('/', isLoggedIn, async(req, res) => {
     const equiposTelco = await pool.query('SELECT * FROM equipostelco');
     res.render('equiposTelco/list', {equiposTelco});
 
     // res.send('listado de los equipos');
 })
 
-router.get('/delete/:Id_ActivoTx', async(req, res) => {
+router.get('/delete/:Id_ActivoTx', isLoggedIn, async(req, res) => {
     const {Id_ActivoTx} = req.params;
     await pool.query('DELETE FROM equiposTelco WHERE Id_ActivoTx = ?', [Id_ActivoTx]);
     req.flash('success','equipo eliminado satisfactoriamente!!');
     res.redirect('/equiposTelco');
 
 })
-router.get('/edit/:Id_ActivoTx', async(req, res) => {
+router.get('/edit/:Id_ActivoTx', isLoggedIn, async(req, res) => {
     const {Id_ActivoTx} = req.params;
     const equipoTelco = await pool.query('SELECT * FROM equipostelco WHERE Id_ActivoTx = ?', [Id_ActivoTx]);
     console.log(equipoTelco[0]);
     res.render('../views/equiposTelco/edit',{equipoTelco: equipoTelco[0]});
 })
-router.post('/edit/:Id_ActivoTx', async(req, res) => {
+router.post('/edit/:Id_ActivoTx', isLoggedIn, async(req, res) => {
     const {Id_ActivoTx} = req.params;
     const {Serial_EqTx, Mac_EqTx, Modelo_EqTx, Marca_EqTx,Description_EqTx, Propietario_EqTx, Ubicación_EqTx } = req.body;
     const editEquipoTelco = { 
